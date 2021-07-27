5584002692b0149eee1a78e33006d7115b24490f
Evald Smalyakov <evaldsmalyakov@mail.ru>
2021-07-27 08:40:17 +0600

    Add: GitHub actions & model offer

--- cmd/ocp-offer-api/main.go
+++ cmd/ocp-offer-api/main.go
@@ -1,7 +1,58 @@
 package main
 
-import "fmt"
+import (
+	"bytes"
+	"fmt"
+	"log"
+	"os"
+)
+
+// Задание: Используя defer и функтор реализовать открытие и закрытие файла в цикле
+//
+// Принемает слайс путей и возвращает слайс считаных файлов
+func FilesReader(files []string) []string {
+	freader := func(fpath string) ([]byte, error) {
+		file, err := os.OpenFile(fpath, os.O_RDONLY, 0)
+		if err != nil {
+			return nil, err
+		}
+
+		defer func() {
+			if err = file.Close(); err != nil {
+				fmt.Printf("Error closing file on path: \"%s\" \n", fpath)
+				log.Fatal(err)
+			} else {
+				fmt.Printf("File on path \"%s\" has successfully closed \n", fpath)
+			}
+		}()
+
+		data := new(bytes.Buffer)
+
+		if _, err = data.ReadFrom(file); err != nil {
+			return nil, err
+		}
+
+		return data.Bytes(), nil
+	}
+
+	result := make([]string, 0)
+
+	for _, file := range files {
+		fbyte, err := freader(file)
+		if err != nil {
+			log.Fatal(err)
+		}
+		result = append(result, string(fbyte))
+	}
+
+	return result
+}
 
 func main() {
+	fpaths := []string{"./main.go", "../../internal/model/offer.go"}
+	files := FilesReader(fpaths)
+	fmt.Println(files)
+
 	fmt.Println("Project: ocp-offer-api")
+
 }
--- internal/utils/model/offer.go
+++ internal/utils/model/offer.go
@@ -3,20 +3,22 @@ package utils
 import (
 	"errors"
 	"math"
+
+	"github.com/ozoncp/ocp-offer-api/internal/model"
 )
 
-// Разделение слайса на батчи - чанки одинкового размера (кроме последнего)
+// Батчевое разделение слайса на слайс слайсов
 //
 // "source" - исходный слайс;
 // "batchSize" - количество частей на которые нужно разбить слайс.
-func Split(source []int, batchSize int) ([][]int, error) {
+func SplitToBulks(source []model.Offer, batchSize uint) ([][]model.Offer, error) {
 	// Проверка на то, что количество батчей больше нуля.
 	if batchSize <= 0 {
 		return nil, errors.New("the batch size must not be less than zero or equal to zero")
 	}
 
 	// Проверка на то, что количество батчей не длиннее размера слайса.
-	if batchSize > len(source) {
+	if batchSize > uint(len(source)) {
 		return nil, errors.New("the batch size is larger than the slice length")
 	}
 
@@ -24,10 +26,10 @@ func Split(source []int, batchSize int) ([][]int, error) {
 	// При передаче в функции слайс копируется, но вместе с указателем на тот же участок памяти,
 	// из-за этого при изменении данных возвращаемого слайса меняется данные и в исходном.
 	// Исходный код слайса https://golang.org/src/runtime/slice.go#L11
-	slice := make([]int, len(source))
+	slice := make([]model.Offer, len(source))
 	copy(slice, source)
 
-	var result [][]int
+	var result [][]model.Offer
 
 	length := len(slice)
 
@@ -41,6 +43,21 @@ func Split(source []int, batchSize int) ([][]int, error) {
 			j = length
 		}
 		result = append(result, slice[i:j])
+	}
+
+	return result, nil
+}
+
+// Конвертации слайса от структуры в отображение,
+// где ключ идентификатор структуры, а значение сама структура
+//
+// "source" - исходный слайс;
+func ConvertSliceToMap(source []model.Offer) (map[uint64]model.Offer, error) {
+
+	result := make(map[uint64]model.Offer, len(source))
+
+	for _, offer := range source {
+		result[offer.Id] = offer
 	}
 
 	return result, nil
